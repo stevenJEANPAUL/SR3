b726734ed35f75b69592ea4d28d28a9e577b2d37
stevenJEANPAUL <99800285+stevenJEANPAUL@users.noreply.github.com>
2022-07-31 19:29:58 +0200

    Ajout AvisClient et Carousel

---
+++ src/Form/AvisClientType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Accueil;
+use App\Entity\AvisClient;
+use Symfony\Component\Form\AbstractType;
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class AvisClientType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('imageName')
+            ->add('updatedAt')
+            ->add('imageFile', FileType::class, ["label"=>"Image :", "required"=>true])
+            ->add('nom', TextType::class, ["label"=>"Nom :"])
+            ->add('poste')
+            ->add('commentaire')
+            ->add('accueils', EntityType::class,["class"=>Accueil::class, "label"=>"Accueils", "multiple"=>true, "required"=>false])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => AvisClient::class,
+        ]);
+    }
+}
