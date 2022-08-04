4f5cb7fda668cff1d48b730e3b66d60c552f9bef
steven <sacha.chadoes@gmail.com>
2022-08-04 18:03:25 +0200

    Update NosActusFixtures.php

--- src/DataFixtures/NosActusFixtures.php
+++ src/DataFixtures/NosActusFixtures.php
@@ -8,7 +8,7 @@ use Doctrine\Persistence\ObjectManager;
 
 class NosActusFixtures extends Fixture
 {
-    public const nouveaute = "detail";
+
     public function load(ObjectManager $manager): void
     {
         
